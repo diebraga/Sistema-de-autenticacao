f1eba35a17a80fe514db4c91bb82920ee1f461e0
diebraga <diebraga0055@gmail.com>
2021-03-18 10:23:22 +0000

    inytegracao babylon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import { Flex, Center } from "@chakra-ui/react"
-
+import BabylonNext from '../components/BabylonNext'
 
 const Index = () => {
   return (
     <Flex height="100vh" bg="gray.800" color="gray.300" alignItems="center" justifyContent="center">
-      <Center>
-      Hello World
-      </Center>
+        <BabylonNext />
     </Flex>
   )
 }
