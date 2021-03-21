67f42f48a04013adb4e84e7958b410ca0e9486c2
diebraga <diebraga0055@gmail.com>
2021-03-21 18:01:30 +0000

    exemplos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import { Flex, Center } from "@chakra-ui/react"
-import BabylonNext from '../components/BabylonNext'
-import ExampleContainer from "../containers/Example-02-container"
+import { Flex } from "@chakra-ui/react"
+import RenderizarObjeto from "../containers/RenderizarObjeto"
 
 const Index = () => {
   return (
     <Flex height="100vh" bg="gray.800" color="gray.300" alignItems="center" justifyContent="center">
-      <ExampleContainer />
+      <RenderizarObjeto />
     </Flex>
   )
 }
